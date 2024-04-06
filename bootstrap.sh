d890e20db7e9721b67795e6767bd5b367395ab5c
sidosera <sidoserah.gmail.com>
2024-04-06 21:16:28 +0100

    default container user

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export HOST_WORKSPACE_DIR=$(pwd)
-export CONTAINER_NAME=bootstrap
+export CN=bootstrap
+export WD=$(pwd)
 
-docker-compose up
+docker-compose up -d
+docker exec $CN /bin/bash -c "adduser $CN; usermod -a -G sudo $CN; echo -e '$CN\n$CN' | passwd  $CN"
